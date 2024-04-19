cb16aece60b63cc2a72dc2e56b3ae4ac3f9c287d
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-04-19 16:45:25 +0100

    Refactor BunnyFile struct in bunny_file.rs to use PascalCase rename

--- src/bunny_file.rs
+++ src/bunny_file.rs
@@ -1,35 +1,21 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "PascalCase")]
 pub struct BunnyFile {
-    #[serde(rename = "Guid")]
     pub guid: String,
-    #[serde(rename = "StorageZoneName")]
     pub storage_zone_name: String,
-    #[serde(rename = "Path")]
     pub path: String,
-    #[serde(rename = "ObjectName")]
     pub object_name: String,
-    #[serde(rename = "Length")]
     pub length: u64,
-    #[serde(rename = "LastChanged")]
     pub last_changed: String,
-    #[serde(rename = "ServerId")]
     pub server_id: i32,
-    #[serde(rename = "ArrayNumber")]
     pub array_number: i32,
-    #[serde(rename = "IsDirectory")]
     pub is_directory: bool,
-    #[serde(rename = "UserId")]
     pub user_id: String,
-    #[serde(rename = "ContentType")]
     pub content_type: String,
-    #[serde(rename = "DateCreated")]
     pub date_created: String,
-    #[serde(rename = "StorageZoneId")]
     pub storage_zone_id: i32,
-    #[serde(rename = "Checksum")]
     pub checksum: Option<String>,
-    #[serde(rename = "ReplicatedZones")]
     pub replicated_zones: Option<String>,
 }
